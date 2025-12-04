e495d6a6929569c4e40744ee7a726ed87d09957e
Tuong Anh <tuonganhdev186@gmail.com>
2025-12-04 21:54:03 +0700

    added video keo mo

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function toggleMenu() {
     const menu = document.getElementById("navMenu");
     menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
 }
+function playVideo(box) {
+    box.innerHTML = `
+        <video src="/images/video-keo-mo.mp4" controls autoplay style="width:100%; height:auto;"></video>`;
+}
